df165151b22fc7e8e9e89661390a34e4a0f46ec4
Elisey <nutellaelik@gmail.com>
2025-01-21 10:55:37 +0100

    Update Risolutore_Equazioni.py
    Final relase

--- Risolutore_Equazioni.py
+++ Risolutore_Equazioni.py
@@ -1,45 +1,51 @@
 #Risolvere un' equazione di secondo grado
-
 import os
-#Ci servono i coefficenti a b e c reali
+from math import sqrt, pow
+ #Ci servono i coefficenti a b e c reali
 def acquisisci_coefficienti():
     a = float(input("Inserisci coefficiente a: "))
     b = float(input("Inserisci coefficiente b: "))
     c = float(input("Inserisci coefficiente c: "))
     return [a, b, c]
- 
+
 #Definisco una funzione che risolvi le equazioni di secondo grado
 def risolvi_I_grado(b, c):
-    if b==0 and c==0:
-        print("Questa equazione è indeterminata")
+    if b == 0 and c == 0:
+        print("Equazione indeterminata")
     elif b == 0:
-        print("Questa equazione è impossibile")
+        print("Equazione impossibile")
     else:
-        print(f"soluzione: x= -c/b={-c/b}")
-    
+        x = -c/b
+        print(f"La soluzione dell'equazione è {x}")
+
 #Definisco una funzione che risolvi le equazioni di secondo grado
 def risolvi_II_grado(a, b, c):
-    delta = (b ** 2)- 4*a*c
-    
+    delta = pow(b,2) - 4 * a * c
+ 
     if delta < 0:
         print("Non esistono soluzioni reali")
-    elif delta ==0:
-        x1 = x2 = -b/(2*a)
-        print(f"Soluzioni reali e coincidenti: \nx1 = {x1}\n x2={x2} ")
-    else:
-        x1 = (-b + delta ** (0.5)) / (2*a)
-        x2 = (-b - delta **(0.5)) / (2*a)
-        print(f"Soluzioni reali e distinte\nx1 = {x1}\nx2 = {x2}")
-        
-scelta=""
+ 
+    elif delta == 0:
+        print("Le due soluzioni reali sono coincidenti")
+        x = -b / (2 * a)
+        print(f"La soluzione dell'equazione è {x}")
+ 
+    elif delta > 0:
+        print("Le due soluzioni reali sono distinte")
+        x1 = (-b - sqrt(delta)) / (2 * a)
+        x2 = (-b + sqrt(delta)) / (2 * a)
+        print(f"Le due soluzioni sono: {x1} e {x2}")
+ 
+scelta = ""
+ 
 while scelta.lower() != "exit":
+ 
     a, b, c = acquisisci_coefficienti()
     if a == 0:
-        print("Equazione di I grado: ")
         risolvi_I_grado(b, c)
     else:
-        print("Equazione di II grado: ")
         risolvi_II_grado(a, b, c)
-    scelta = input("premi un tasto per continuare, exit per terminare... \n")
+    scelta = input("Premi un tasto per continuare, exit per terminare...\n")
     os.system("cls")
+ 
 print("Programma terminato correttamente")
